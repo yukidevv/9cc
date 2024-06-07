1d479e11fc786e9d504124741d986c9e3bd17239
yukidevv <nomad048@gmail.com>
2024-06-07 19:38:12 +0900

    update

--- m1/docker_util.sh
+++ m1/docker_util.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-if [ $1 = "in" ]; then
+#中に入りたい
+if [[ $1 = "in" ]]; then
   docker run --rm -it -v $HOME/fun/9cc:/9cc compilebook
 else
-  docker run --rm -v $HOME/fun/9cc:/9cc -w /9cc compilebook make
+  docker run --rm -v $HOME/fun/9cc:/9cc -w /9cc compilebook make test
 fi
 
